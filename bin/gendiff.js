06a72658ff89e9f02f6ecc5b3acd13eaad3bc96d
Vitaly Klyukin <delphython@gmail.com>
2023-11-12 12:02:02 +0300

    add format option & override help text

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,7 +6,11 @@ const program = new Command();
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1');
+  .version('0.0.1')
+  .option('-f, --format', 'output format')
+  .helpOption('-h, --help', 'output usage information')
+  .argument('<filepath1>', '')
+  .argument('<filepath2>', '');
 
 program.parse(process.argv);
 
