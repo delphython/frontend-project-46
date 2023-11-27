094cd8ec9300d185013372aef28e7c482060bde4
Vitaly Klyukin <delphython@gmail.com>
2023-11-27 15:43:48 +0300

    add absolute path

--- src/parsefile.js
+++ src/parsefile.js
@@ -1,7 +1,11 @@
 import {readFileSync} from 'node:fs';
+import { cwd } from 'node:process';
+import { resolve } from 'node:path';
 
 export const printJson = (inputFilePath) => {
-    const inputFile = readFileSync(inputFilePath);
+    const absPAth = resolve(cwd(), inputFilePath);
+    
+    const inputFile = readFileSync(absPAth);
     
     const inputFileContent = JSON.parse(inputFile);
 
