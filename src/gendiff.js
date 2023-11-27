db1f019292dddb3afd1bcc7a18576454f3ff4cd2
Vitaly Klyukin <delphython@gmail.com>
2023-11-27 15:34:46 +0300

    rebuild app

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,21 +1,7 @@
 import app from './index.js';
-import printJson from './index.js';
 import { Command } from "commander";
 
 const program = new Command();
-
-program
-  .name('gendiff')
-  .description('Compares two configuration files and shows a difference.')
-  .version('0.0.1')
-  .option('-f, --format', 'output format')
-  .helpOption('-h, --help', 'output usage information')
-  .argument('<filepath1>', '')
-  .argument('<filepath2>', '')
-  .action((filepath1, filepath2) => {
-    printJson(filepath1);
-    printJson(filepath2);
-  });
 
 export default () => {
   app(program);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 #!/usr/bin/env node
 
-const app = (program) => {
-    program.parse(process.argv);
+import {printJson} from './parsefile.js'
 
+const app = (program) => {
+    program
+        .name('gendiff')
+        .description('Compares two configuration files and shows a difference.')
+        .version('0.0.1')
+        .option('-f, --format', 'output format')
+        .helpOption('-h, --help', 'output usage information')
+        .argument('<filepath1>', '')
+        .argument('<filepath2>', '')
+        .action((filepath1, filepath2) => {
+            printJson(filepath1);
+            printJson(filepath2);
+        });
+  
+    program.parse(process.argv);
     if (!program.args.length) program.help();
 };
   
