6cb8a9248c7a34b1aea319e74833f697f332eb8c
Vitaly Klyukin <delphython@gmail.com>
2023-12-03 16:14:47 +0300

    add genDiff

--- src/parsefile.js
+++ src/parsefile.js
@@ -1,6 +1,7 @@
 import {readFileSync} from 'node:fs';
 import { cwd } from 'node:process';
 import { resolve } from 'node:path';
+import _ from 'lodash';
 
 const getFileContent = (inputFilePath) => {
     var inputFileContent = '';
@@ -8,7 +9,7 @@ const getFileContent = (inputFilePath) => {
     const absPAth = resolve(cwd(), inputFilePath);
 
     const fileExt = absPAth.split('.').pop();
-    
+
     const inputFile = readFileSync(absPAth);
 
     if (fileExt === 'json') {
@@ -18,7 +19,54 @@ const getFileContent = (inputFilePath) => {
     return inputFileContent;
 };
 
+const getDiffInSecondFile = (obj1, obj2) => {
+    const result = Object.entries(obj2).filter(
+        ([field, obj2Value]) => obj1[field] !== obj2Value
+    );
+
+    result.forEach((_, index) => {
+        result[index].push('+');
+        result[index].push(2);
+    });
+
+    return result;
+};
+
+const getDiffInFirstFile = (obj1, obj2) => {
+    const result = Object.entries(obj1).filter(
+      ([field, obj1Value]) => obj2[field] !== obj1Value
+    );
+
+    result.forEach((_, index) => {
+        result[index].push('-');
+        result[index].push(1);
+    });
+
+    return result;
+};
+
+const getEqInBothFiles = (obj1, obj2) => {
+    const result = Object.entries(obj2).filter(
+      ([field, obj2Value]) => obj1[field] === obj2Value
+    );
+
+    result.forEach((_, index) => {
+        result[index].push(' ');
+        result[index].push(3);
+    });
+
+    return result;
+};
+
 export const genDiff = (inputFile1, inputFile2) => {
-  console.log(getFileContent(inputFile1));
-  console.log(getFileContent(inputFile2));
+    const file1Content = getFileContent(inputFile1);
+    const file2Content = getFileContent(inputFile2);
+    
+    const diffInSecondFile = getDiffInSecondFile(file1Content, file2Content);
+    const diffInFirstFile = getDiffInFirstFile(file1Content, file2Content);
+    const eqInBothFiles = getEqInBothFiles(file1Content, file2Content);
+
+    const diff = [...diffInSecondFile, ...diffInFirstFile, ...eqInBothFiles];
+    const sortedDiff = _.sortBy(diff, [(obj) => [obj[0], obj[3]]]);
+    console.log(sortedDiff);
 };
