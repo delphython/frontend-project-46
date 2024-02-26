fa8fe3a6e175ed9b73fdbe33c17105c1f73be6cd
Vitaly Klyukin <delphython@gmail.com>
2024-02-26 16:01:21 +0300

    add plain formatter

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,9 +1,13 @@
 import stylish from './stylish.js';
+import plain from './plain.js';
 
 const format = (tree, formatName) => {
   switch (formatName) {
     case 'stylish': {
       return stylish(tree);
+    }
+    case 'plain': {
+      return plain(tree);
     }
     default:
       throw new Error('This formatting is not in use.');
