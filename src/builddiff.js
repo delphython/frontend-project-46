97f78d7d23ee3a16f44f5ea62f3a5221cac842c8
Vitaly Klyukin <delphython@gmail.com>
2024-02-26 13:25:58 +0300

    fix buikddiff

--- src/builddiff.js
+++ src/builddiff.js
@@ -1,46 +1,43 @@
 import _ from 'lodash';
 
 const buildDiff = (obj1, obj2) => {
-  const objKeys1 = _.keys(obj1);
-  const objKeys2 = _.keys(obj2);
-
-  const sortedKeys = _.sortBy(_.union(objKeys1, objKeys2));
+  const sortedKeys = _.sortBy(_.union(_.keys(obj1), _.keys(obj2)));
 
   const result = sortedKeys.map((key) => {
-    if (!_.has(obj1, key)) {
-      return {
-        name: key,
-        value: obj2[key],
-        type: '+',
-      };
-    }
-    if (!_.has(obj2, key)) {
-      return {
+    switch (true) {
+      case (!_.has(obj1, key)): 
+        return {
+          name: key,
+          value: obj2[key],
+          type: 'added',
+        };
+      case (!_.has(obj2, key)): 
+        return {
+          name: key,
+          value: obj1[key],
+          type: 'deleted',
+        };
+      case (_.isObject(obj1[key]) && _.isObject(obj2[key])):
+        return {
+          name: key,
+          type: 'nested',
+          children: buildDiff(obj1[key], obj2[key]),
+        };
+      case (obj1[key] !== obj2[key]):
+        return {
+          name: key,
+          value1: obj1[key],
+          value2: obj2[key],
+          type: 'changed',
+        };
+      default: return {
         name: key,
         value: obj1[key],
-        type: '-',
+        type: 'unchanged',
       };
     }
-    if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
-      return {
-        name: key,
-        type: '_',
-        children: buildDiff(obj1[key], obj2[key]),
-      };
-    }
-    if (obj1[key] !== obj2[key]) {
-      return {
-        name: key,
-        value: obj2[key],
-        type: ' ',
-      };
-    }
-    return {
-      name: key,
-      value: obj1[key],
-      type: ' ',
-    };
   });
+
   return result;
 };
 
