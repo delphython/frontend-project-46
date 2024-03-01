7ccf644b5b21e365e88287d956b04313d4cf3a96
Vitaly Klyukin <delphython@gmail.com>
2024-03-01 22:48:50 +0300

    fix lint errors

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -13,7 +13,7 @@ program
   .argument('<filepath1>', '')
   .argument('<filepath2>', '')
   .action((filepath1, filepath2, options) => {
-      genDiff(filepath1, filepath2, options.format);
+    genDiff(filepath1, filepath2, options.format);
   });
 
 program.parse();
